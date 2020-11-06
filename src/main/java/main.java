c1ffa7a1dbf59e3e77c4d4cc29e177702fbbacdd
Student   PRIOR-HARDY Chloe <06964@farnborough.ac.uk>
2020-11-06 10:05:49 +0000

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        String greeting = "hello world!";
+        System.out.println(greeting);
+        System.out.println(greeting.length());
+    }
+}
+
